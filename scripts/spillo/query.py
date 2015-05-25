b70afbf35270ee3bbfc94304be29094bd8a70545
Damien DeVille <damien@ddeville.me>
2015-05-25 18:21:15 +0100

    Use properties

--- scripts/spillo/query.py
+++ scripts/spillo/query.py
@@ -6,19 +6,39 @@ class Query(object):
 
     QueryException = QueryException
 
-    def __init__(self, query_string):
-        self.value = None
-        self.name = None
-        self.url = None
-        self.tags = None
-        self.desc = None
+    _value = None
+    _name = None
+    _url = None
+    _tags = None
+    _desc = None
 
+    def __init__(self, query_string):
         # try to parse the arguments, if an exception is thrown it's because some args
         # are incomplete and we shouldn't return any result until they are
         try:
             specific = self._parse_query_string(query_string)
         except:
             raise Query.QueryException()
+
+    @property
+    def value(self):
+        return self._value
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def url(self):
+        return self._url
+
+    @property
+    def tags(self):
+        return self._tags
+
+    @property
+    def desc(self):
+        return self._desc
 
     def _parse_query_string(self, query_string):
         parser = _QueryParser()
@@ -30,11 +50,11 @@ class Query(object):
 
         args = vars(parser.parse_args(query_string.split()))
 
-        self.value = ' '.join(args['value']) if args['value'] else None
-        self.name = ' '.join(args['name']) if args['name'] else None
-        self.url = ' '.join(args['url']) if args['url'] else None
-        self.desc = ' '.join(args['desc']) if args['desc'] else None
-        self.tags = args['tags']
+        self._value = ' '.join(args['value']) if args['value'] else None
+        self._name = ' '.join(args['name']) if args['name'] else None
+        self._url = ' '.join(args['url']) if args['url'] else None
+        self._desc = ' '.join(args['desc']) if args['desc'] else None
+        self._tags = args['tags']
 
 class _QueryParser(argparse.ArgumentParser):
     def _get_action_from_name(self, name):
